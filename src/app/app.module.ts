84559708697d2c8d975a9fe60aa6c6ddd7b03c5e
lee <edy5016@navet.com>
2018-03-17 11:07:36 +0900

    라울팅테이블 설계

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
+import * as path from 'path';
 
+// 라우팅 테이블
+const routes : Routes = [
+  {path:'', component: IndexComponent, children:[   // children 가질수 있는이유는 indexcomponent안에 route-outlet 이 있기떄문에
+      {path:'',component : HomeComponent},
+      {path:'jquery',component:JqueryComponent},
+      {path:'angular',component:AngularComponent}
+    ]}, /*사용자회면 app컴포넌트탐 */
+  // 참고: 향후 관리자 생성 모듈
+  // { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
